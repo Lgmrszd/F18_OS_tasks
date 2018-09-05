ee6929269973b7d26690b40e67aafb1388ad07d7
Mikhail <m.v.bobrov@yandex.ru>
2018-09-05 20:33:48 +0300

    week 3

---
+++ week3/ex3.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct LinkedNode {
+    int data;
+    struct LinkedNode* next;
+} LinkedNode;
+
+typedef struct {
+    struct LinkedNode* head;
+} LinkedList;
+
+void print_list(LinkedList* list){
+    LinkedNode* node = list->head;
+    if (node == NULL) return;
+    printf("%d", node->data);
+    while (node->next != NULL) {
+        node = node->next;
+        printf(" %d", node->data);
+    }
+    printf("\n");
+}
+
+LinkedNode *create_node(int data){
+    LinkedNode *node = (LinkedNode*) malloc(sizeof(LinkedNode));
+    node->data = data;
+    node->next = NULL;
+}
+
+void insert_node(LinkedList *list, int index, int data){
+    LinkedNode* node = list->head;
+    LinkedNode* new_node = create_node(data);
+    if (node == NULL) {
+        list->head = new_node;
+        return;
+    }
+    if (index < 1) {
+        new_node->next = node;
+        list->head = new_node;
+        return;
+    }
+    index--;
+    while (node->next != NULL && index>0) {
+        node = node->next;
+        index--;
+    }
+    new_node->next = node->next;
+    node->next = new_node;
+}
+
+void delete_node(LinkedList *list, int index) {
+    if (list->head == NULL) return;
+    if (index < 1) {
+        LinkedNode *old_node = list->head;
+        list->head = old_node->next;
+        free(old_node);
+        return;
+    }
+    LinkedNode* node = list->head;
+    index--;
+    while (node->next != NULL && index>0) {
+        node = node->next;
+        index--;
+    }
+    LinkedNode* next_node = node->next;
+    if (next_node != NULL) {
+        node->next = next_node->next;
+        free(next_node);
+    }
+}
+
+void append_node(LinkedList *list, int data){
+    LinkedNode* node = list->head;
+    LinkedNode* new_node = create_node(data);
+    if (node == NULL) {
+        list->head = new_node;
+        return;
+    }
+    while (node->next != NULL) {
+        node = node->next;
+    }
+    node->next = new_node;
+}
+
+LinkedList *create_list(){
+    LinkedList *list = (LinkedList*) malloc(sizeof(LinkedList));
+    list->head = NULL;
+    return list;
+}
+
+int main(){
+    LinkedList *list = create_list();
+    append_node(list, 0);
+    append_node(list, 1);
+    append_node(list, 2);
+    append_node(list, 3);
+    //insert_node(list, 0, 100);
+    delete_node(list, 1);
+    print_list(list);
+    return 0;
+}
+
