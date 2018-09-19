8b249d1b91fccc669f812ecf437986cc38dd7504
Mikhail <m.v.bobrov@yandex.ru>
2018-09-19 22:58:30 +0300

    week 5 ex 2

---
+++ week5/ex2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Executing just "./ex2.sh" gives "501" as last line
+# Executing ./ex2.sh & ./ex2.sh gives "1001"
+
+FILE="ex2.txt"
+FILELOCK=$FILE.lock
+# rm $FILE.lock
+
+echo '1' > $FILE
+
+N=500
+while [ $N -gt 0 ]
+do
+    if $(ln $FILE $FILELOCK 2> /dev/null); then
+        echo $(($(tail -n 1 $FILE) + 1)) >> $FILELOCK
+        rm $FILELOCK
+        N=$(($N-1))
+    fi
+done
+
