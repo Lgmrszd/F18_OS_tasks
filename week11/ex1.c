e5d895e2e584d6ff2a3e060405db94016359d0a2
Mikhail <m.v.bobrov@yandex.ru>
2018-10-31 15:45:54 +0300

    week 11 ex 1

---
+++ week11/ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+//#include <sys/stat.h>
+
+int main() {
+    char *str_data = "This is a nice day\n";
+    int str_size = strlen(str_data);
+    int fd;
+    void *fm;
+    fd = open("ex1.txt", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    lseek(fd, str_size+1, SEEK_SET);
+    write(fd, "", 1);
+    fm = mmap(NULL, str_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    memcpy(fm, str_data, str_size);
+    return 0;
+}
---
+++ week11/ex1.sh
@@ -0,0 +1,15 @@
+gcc ex1.c -o ex1
+echo "Short file test"
+echo "a" > ex1.txt
+./ex1
+cat ex1.txt
+echo "Long file test"
+echo "testtesttesttesttesttesttesttesttesttest\ntesttest" > ex1.txt
+./ex1
+cat ex1.txt
+echo "File does not exist test"
+rm ex1.txt
+./ex1
+cat ex1.txt
+rm ex1
+rm ex1.txt
